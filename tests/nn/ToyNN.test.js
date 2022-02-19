a59270082ccf194e93a96cff8460f89b3f37429c
vgvm <luckybit4755@gmail.com>
2022-02-19 13:10:24 -0500

    refactor verification, add gradient descent tests and work more with the distance network (still not working)

--- tests/nn/ToyNN.test.js
+++ tests/nn/ToyNN.test.js
@@ -4,26 +4,44 @@ const nj = require('../../src/util/njeez');
 const ToyNN = require( '../../src/nn/ToyNN' );
 const DataBuddy = require('../../src/util/DataBuddy');
 
+const verifyLabels = ( labels, predictions, threshold = .1, verbose = false ) => {
+	predictions = predictions.selection.data;
+	if ( verbose ) {
+		console.log( 'labels      :', labels );
+		console.log( 'predictions  :', predictions );
+	}
+	expect( labels.length ).toEqual( predictions.length );
+	predictions.forEach( (prediction,i) => {
+		const label = labels[ i ];
+		const diff = Math.abs( prediction - label );
+		if ( verbose ) {
+			console.log( 'prediction', prediction, 'label', label );
+		}
+       	expect( diff ).toBeLessThan( threshold );
+	});
+
+};
+
 test('toynn-column0',()=> {
 	const traskColumn0 = new ToyNN( 3, 1 );
 
     const trainingData = DataBuddy.createColumn0TrainingData();
     const trainingResult = traskColumn0.train( trainingData );
-    console.log( trainingResult.selection.data.map( Math.round ).join( ' ' ) );
 
     const validationData = DataBuddy.createColumn0TrainingData();
-    const validation = traskColumn0.predict( validationData.inputs );
+    const predictions = traskColumn0.predict( validationData.inputs );
+	verifyLabels( validationData.labels, predictions, .2 );
+});
 
-	const simplified = validation.selection.data.map( Math.round );
-    console.log( simplified.join( ' ' ) );
+test('toynn-column0-gd',()=> {
+	const traskColumn0 = new ToyNN( 2, 1 );
 
-	simplified.forEach( (label,i) => {
-		if ( i % 2 ) {
-			expect( label ).toBeGreaterThan( .99 );
-		} else {
-       		expect( label ).toBeLessThan(.01);
-		}
-	});
+    const trainingData = DataBuddy.createColumn0C2TrainingData();
+    const trainingResult = traskColumn0.trainGradientDescent( trainingData );
+
+    const validationData = DataBuddy.createColumn0C2TrainingData();
+    const predictions = traskColumn0.predict( validationData.inputs );
+	verifyLabels( validationData.labels, predictions );
 });
 
 test('toynn-xor',()=> {
@@ -31,37 +49,54 @@ test('toynn-xor',()=> {
 
     const trainingData = DataBuddy.createXorTrainingData();
     const trainingResult = traskXor.train( trainingData );
-    console.log( trainingResult.selection.data.map( Math.round ).join( ' ' ) );
 
     const validationData = DataBuddy.createXorTrainingData();
-    const validation = traskXor.predict( validationData.inputs );
-
-	const simplified = validation.selection.data.map( Math.round );
-    console.log( simplified.join( ' ' ) );
-
-	simplified.forEach( (label,i) => {
-		const data = validationData.inputs[ i ];
-		if ( 1 == data[ 0 ] +  data[ 1 ] ) {
-			expect( label ).toBeGreaterThan( .99 );
-		} else {
-       		expect( label ).toBeLessThan(.01);
-		}
-	});
+    const predictions = traskXor.predict( validationData.inputs );
+	verifyLabels( validationData.labels, predictions );
 });
 
+test('toynn-xor-gd',()=> {
+	const traskXor = new ToyNN( 3, 4, 1 );
+
+    const trainingData = DataBuddy.createXorTrainingData();
+    const trainingResult = traskXor.trainGradientDescent( trainingData );
+
+    const validationData = DataBuddy.createXorTrainingData();
+    const predictions = traskXor.predict( validationData.inputs );
+	verifyLabels( validationData.labels, predictions );
+});
+
+// this just really doesn't seem to work... perhaps the toy implementation
+// is still too weak...
 test('toynn-distance',()=> {
-	const toyDistance = new ToyNN( 3, 6, 1 );
+	//const toyDistance = new ToyNN( 3, 3, 1 );
+	const toyDistance = new ToyNN( 3, 1 );
 
-	const trainingData = DataBuddy.createDistanceTrainingData( 1000 );
-	const trainingResult = toyDistance.train( trainingData, 1000 * 100 );
+	const sort = true;
 
-	const validationData = DataBuddy.createDistanceTrainingData( 1000 );
-	const validation = toyDistance.predict( validationData.inputs );
+	const trainingData = DataBuddy.createDistanceTrainingData( 1000, sort );
+	//const trainingResult = toyDistance.train( trainingData, 1000 * 10 );
+	const trainingResult = toyDistance.trainGradientDescent( trainingData, .33, 1000 );
+
+	const validationData = DataBuddy.createDistanceTrainingData( 1000, sort );
+	const predictions = toyDistance.predict( validationData.inputs );
 
 	console.log( 
-		'training   variance is', DataBuddy.variance( trainingResult.selection.data, trainingData.labels ) 
+		'training    variance is', DataBuddy.variance( trainingResult.selection.data, trainingData.labels ) 
 		+ '\n' +
-		'validation variance is', DataBuddy.variance( validation.selection.data, validationData.labels ) 
+		'predictions variance is', DataBuddy.variance( predictions.selection.data, validationData.labels ) 
 	);
+
+	toyDistance.weights.forEach( (w,i) => console.log( `w${i} =`, nj.toArray( w.T ) ) );
+
+	const output = nj.array( predictions.selection.data );
+	const labels = nj.array( validationData.labels );
+
+	const error = nj.mean( nj.abs( nj.subtract( output, labels ) ) );
+	console.log( 'average error is', error );
+
+
+	// this is borked...
+	//verifyLabels( validationData.labels, validation, .3 );
 });
 
