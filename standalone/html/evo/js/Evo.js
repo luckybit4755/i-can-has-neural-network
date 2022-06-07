1e8fdd0fb0e3a70d7703ccecfc982154eb4d24d5
vgvm <luckybit4755@gmail.com>
2022-06-07 17:11:13 -0400

    refactor and a few experiments

--- standalone/html/evo/js/Evo.js
+++ standalone/html/evo/js/Evo.js
@@ -12,7 +12,7 @@ class Evo extends Drawing {
 	// 0> 4:43, 7:90, 5,4,6:107
 	// 0> 3:74, 4:47, 7:51, 5,4,6:80  (bias and memory from here)
 	// 1> 3:36, 4:48, 7:70, 5,4,6:96  
-	// 2> 3:58, 4:60, 7:41, 5,4,6:79  <- percents
+	// 2> 3:58, 4:60, 7:41, 5,4,6:79, 6,4:95%@145  <- percents
 	configure( 
 		hidden = [3,3], 
 		size = 128, 
@@ -24,6 +24,7 @@ class Evo extends Drawing {
 		maxGenerations = 128,
 		mutationRate = 1 / 1000
 	) {
+		this.hidden = hidden;
 		this.size = size;
 		this.nubCount = nubCount;
 		this.generation = generation;
@@ -44,23 +45,35 @@ class Evo extends Drawing {
 		this.trail = 4; 
 
 		this.fps = 44;
+		this.reset();
+	}
 
+	reset() {
 		this.survivalCounts = [];
 
 		this.done = false; // and with strange aeons ...
 		this.summarized = false; 
 
 		// everyone loves counters!
-		this.lastSurviverCount = 0;
-		this.sarnathCounter = 0;
 		this.iteration = 0;
 		this.paused = 0;
+		this.sarnathCounter = 0;
 
+		this.skipRequested = false;
 		this.apotheosized = false;
 
+		this.lastBest = 0;
+
 		// here they are!
-		this.nubs = new Array( this.nubCount ).fill( 0 ).map( _=>new Nub( hidden ) );
+		this.nubs = new Array( this.nubCount ).fill( 0 ).map( _=>new Nub( this.hidden ) );
 		this.board = new Board( this.nubs, this.size );
+
+		/*
+		// super slow and weird... idk... may need a rewrite to leverage it...
+		this.gpuKernels = this.nubs[ 0 ].createGpuKernels();
+		console.log('hi', this.gpuKernels );
+		this.nubs.forEach( nub=>nub.gpuKernels = this.gpuKernels );
+		*/
 
 		return this;
 	}
@@ -84,8 +97,12 @@ class Evo extends Drawing {
 
 		this.clear();
 		this.drawMechanism();
-		
-		this.move();
+
+		if ( this.skipRequested ) {
+			this._skip();
+		} else {
+			this.move();
+		}
 
 		if ( this.done && !this.summarized ) {
 			this.summarized = true;
@@ -114,7 +131,7 @@ class Evo extends Drawing {
 			if ( this.doom() ) {
 				this.sarnath();
 				if ( this.iterationsPerFrame > this.generation ) {
-					this.paused = this.fps * .02 + 1;
+					this.paused = this.fps * .01 + 1;
 				} else {
 					this.paused = this.fps;
 				}
@@ -142,16 +159,15 @@ class Evo extends Drawing {
 	}
 
 	skip( count = 10 ) {
+		this.skipRequested = count;
+	}
+
+	_skip() {
+		const count = this.skipRequested;
+		this.skipRequested = false;
+
 		const y = this.h * .93;
-
-		if ( this.paused>0 ) {
-			this.fillText( 'could not skip', 12, y );
-			return; // gross
-		}
 			
-		clearTimeout( this.timeout );
-		this.paused = 99999;
-
 		this.fillText( `skipping from ${this.iteration}`, 12, y );
 
 		for ( let i = 0 ; i < count && !this.done ; i++ ) {
@@ -167,9 +183,6 @@ class Evo extends Drawing {
 			}
 		}
 		this.fillText( `to ${this.iteration}`, 33, y );
-		this.paused = -1;
-		this.nextFrame();
-		console.log( 'giddy up...' );
 	}
 
 	doom() {
@@ -177,8 +190,8 @@ class Evo extends Drawing {
 	}
 
 	sarnath() {
-		const winners = this.nubs.filter( nub => this.survived( nub ) );
-		this.lastSurviverCount = winners.length;
+		let winners = this.nubs.filter( nub => this.survived( nub ) );
+
 		this.sarnathCounter = this.generation / this.iterationsPerFrame * .33 + 3;
 
 		if ( winners.length < 2 ) {
@@ -188,6 +201,17 @@ class Evo extends Drawing {
 		}
 
 		const percent = Math.floor( 100 * winners.length / this.nubCount );
+
+		/*
+		// experimental
+		if ( percent > this.lastBest ) {
+			this.lastBest = percent;
+			this.bestWinners = winners;
+		} else {
+			winners = this.bestWinners;
+			console.log( percent, '<', this.lastBest );
+		}
+		*/
 
 		this.survivalCounts.push( percent );
 
@@ -221,6 +245,8 @@ class Evo extends Drawing {
 		});
 
 		this.nubs = nextGeneration;
+		this.nubs.forEach( nub=>nub.gpuKernels = this.gpuKernels );
+
 		this.placeNubs();
 	}
 
@@ -273,7 +299,7 @@ class Evo extends Drawing {
 		const w = this.w * .5;
 		const h = this.h * .5;
 
-		// this.debugMechanism();
+		//return this.debugMechanism();
 
 		switch( this.mechanism ) {
 			case 1:  this.fillRect( this.w * .75 ); break
@@ -284,7 +310,7 @@ class Evo extends Drawing {
 				this.line( 0,0, this.w,this.h );
 				this.line( 0,this.h, this.w,0 );
 				break;
-			default: this.fillRect( 0, this.h * .5 ); 
+			default: this.fillRect( 0, this.h * .666 ); 
 		}
 	}
 
@@ -300,13 +326,14 @@ class Evo extends Drawing {
 		const h = this.h * .5;
 
 		switch( this.mechanism ) {
-			case 1: return x > w * .75;
+			case 1: return x > this.w * .75 && x < this.w && 0 < y && y < this.h;
 			case 2: return this.insideCircle( x,y, w,h, h * .5 );
-			case 3: return (
-					( Math.abs( x - y ) < w * .25 )
+			case 3: 
+				return (
+					   ( Math.abs( x - y ) < w * .25 )
 					|| (Math.abs( x - this.w + y ) < w * .25 )
 				)
-			default: return y > h;
+			default: return y > this.h * .666 && y < this.h && 0 < x && x < this.w;
 		}
 	}
 };
